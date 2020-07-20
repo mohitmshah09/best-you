2f3fdd577562f870b203155046d4dfa3f5eb468e
komalsakhiya <komalsakhiya21@gmail.com>
2020-07-20 16:32:39 +0530

    change in verify code design

--- src/app/shared/verification-code/verification-code.page.ts
+++ src/app/shared/verification-code/verification-code.page.ts
@@ -39,18 +39,6 @@ export class VerificationCodePage implements OnInit {
   }
 
 
-  otpController(event, next, prev) {
-    if (event.target.value.length < 1 && prev) {
-      prev.setFocus()
-    }
-    else if (next && event.target.value.length > 0) {
-      next.setFocus();
-    }
-    else {
-      return 0;
-    }
-  }
-
   onOtpChange(otp) {
     this.code = otp;
     console.log("code",this.code)
@@ -61,8 +49,6 @@ export class VerificationCodePage implements OnInit {
    * Verify code
    */
   next() {
-    // console.log("code ", $("#first").val(), $("#second").val(), $("#third").val(), $("#fourth").val());
-    // this.code = $("#first").val().concat($("#second").val()).concat($("#third").val()).concat($("#fourth").val())
     console.log("CODE", this.code, this.verificationCode);
     if (!this.code) {
       this.message = "Code required*";
